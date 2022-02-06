35337dd63c5d73a56fc9a55aaf619dbfa8a32fc9
Vikash Gupta <mrvikash700@gmail.com>
2022-02-06 11:40:59 +0530

    my first commit

---
+++ java/com/coder/RegisterServlet.java
@@ -0,0 +1,26 @@
+package com.coder;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/RegisterServlet")
+public class RegisterServlet extends HttpServlet {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html");
+		
+		PrintWriter out = response.getWriter();
+		
+		out.println("this is the Register Servlet");
+	}
+
+}
